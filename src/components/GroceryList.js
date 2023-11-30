701eb72d78b85ed67caa4873ddcd667bb1c74543
Maja Kadic <mkadic2@student.gdu.edu>
2023-11-30 13:27:13 -0500

    UI mock data

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
-import '../GroceryList.css'; // Ensure this path correctly points to your CSS file
+import '../GroceryList.css'; 
 
 // Mock data simulating items from a database
 const allItems = [
   { id: 1, name: 'Apples', category: 'Produce', cost: 1.2 },
   { id: 2, name: 'Bread', category: 'Bakery', cost: 2.5 },
-  // ... more items
+
 ];
 
 const GroceryList = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [groceryItems, setGroceryItems] = useState([]);
 
-  // Handles search input changes
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  // Adds an item to the grocery list
   const addItemToList = (item) => {
-    setGroceryItems([...groceryItems, { ...item, quantity: 1 }]);
+    setGroceryItems([...groceryItems, { ...item, quantity: 1, purchased: false }]);
   };
 
-  // Removes an item from the grocery list
   const removeItemFromList = (id) => {
     setGroceryItems(groceryItems.filter((item) => item.id !== id));
   };
 
-  // Filters items based on the search term
+  const markItemAsPurchased = (id) => {
+    setGroceryItems(groceryItems.map((item) => 
+      item.id === id ? { ...item, purchased: !item.purchased } : item
+    ));
+  };
+
   const filteredItems = searchTerm
     ? allItems.filter((item) =>
         item.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -44,20 +46,25 @@ const GroceryList = () => {
         onChange={handleSearchChange}
         className="search-box"
       />
-      <ul>
+      <ul className="item-list">
         {filteredItems.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} className="item-tile">
             {item.name} - ${item.cost.toFixed(2)}
-            <button onClick={() => addItemToList(item)}>Add to List</button>
+            <button onClick={() => addItemToList(item)} className="button">Add to List</button>
           </li>
         ))}
       </ul>
       <h2>Your List</h2>
-      <ul>
+      <ul className="grocery-list">
         {groceryItems.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} className={`list-tile ${item.purchased ? 'purchased' : ''}`}>
             {item.quantity} x {item.name} - ${item.cost.toFixed(2)}
-            <button onClick={() => removeItemFromList(item.id)}>Remove</button>
+            <div>
+              <button onClick={() => markItemAsPurchased(item.id)} className="button">
+                {item.purchased ? 'Unmark' : 'Mark as Purchased'}
+              </button>
+              <button onClick={() => removeItemFromList(item.id)} className="button">Remove</button>
+            </div>
           </li>
         ))}
       </ul>
